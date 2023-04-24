649eab143e9682ea68c2ef114017c559cef6fed5
Asier Marqués <asiermarques@gmail.com>
2023-04-24 20:17:14 +0200

    updates

--- src/gatsby-theme-conferencer/components/LogoImage.js
+++ src/gatsby-theme-conferencer/components/LogoImage.js
@@ -3,6 +3,7 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Logo = (alt) => <StaticImage
     src="../../../static/images/logo.png"
+    imgStyle={{ transition: 'none', transform: 'none', willChange: 'inherit' }}
     alt={"Logo DevOpsConf"}
 />;
 
---
+++ src/gatsby-theme-conferencer/components/SpeakersContainer.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import {graphql, useStaticQuery} from "gatsby";
+import Speakers from "gatsby-theme-conferencer/src/components/Speakers";
+
+export default () => {
+    const data = useStaticQuery(graphql`
+          query {
+            allMarkdownRemark(filter: {fields: {collection: {eq: "speaker"}}}, sort: {fileAbsolutePath: ASC}){
+                nodes {
+                  frontmatter {
+                    name
+                    key
+                    image {
+                      childImageSharp {
+                        gatsbyImageData
+                      }
+                    }
+                    title
+                  }
+                }
+              }
+            }
+        `);
+    return <Speakers speakers={data.allMarkdownRemark.nodes.map(node => node.frontmatter)}/>;
+}
